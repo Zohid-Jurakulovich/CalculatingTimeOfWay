63210c6ab3c8f73d0b363717b7db8faea15e6ca4
Zohid-Jurakulovich <zohidjurakulovich@gmail.com>
2022-02-28 23:41:10 +0500

    Add some html, css  and js codes

--- js/main.js
+++ js/main.js
@@ -4,35 +4,75 @@ var elResultPerson = document.querySelector(".resultPerson");
 var elResultBicycle = document.querySelector(".resultBicycle");
 var elResultCar = document.querySelector(".resultCar");
 var elResultAirplane = document.querySelector(".resultAirplane");
+var elResultNotification = document.querySelector(".notification");
 
 
 elForm.addEventListener("submit", function(event){
-event.preventDefault()
-var elInputValue = elInput.value;
-var speedPerson = 3.6;
-var speedBicycle = 20.1;
-var speedCar = 70;
-var speedAirplane = 800;
+  event.preventDefault()
+  var elInputValue = elInput.value;
+  var speedPerson = 3.6;
+  var speedBicycle = 20.1;
+  var speedCar = 70;
+  var speedAirplane = 800;
+  
+  
+  if (elInputValue <= 0 || isNaN(elInputValue)){
+    elResultNotification.textContent = "Iltimos 0 dan katta raqam kiriting, yoki son kiriting"
+    
+    elResultNotification.classList.add("notification-error");
+    elResultNotification.classList.remove("notification-success");
+    
+    return;
+  }else{
+    elResultNotification.textContent = ""
+    elResultNotification.classList.remove("notification-error");
+    
+    
+    
+  }
+  
+  
+  
+  
+  function calculatePersonTime (){
+    var hour = Math.floor(elInputValue / speedPerson);
+    var minute = Math.floor((elInputValue / speedPerson - hour) * 60);
+    var second = Math.floor(((elInputValue / speedPerson - hour) * 60 - minute) * 60);
+    
+    return hour + " soat " + minute + " minut " + second + " sekund"; 
+  }
+  
+  function calculateBicycleTime (){
+    var hour = Math.floor(elInputValue / speedBicycle);
+    var minute = Math.floor((elInputValue / speedBicycle - hour) * 60);
+    var second = Math.floor(((elInputValue / speedBicycle - hour) * 60 - minute) * 60);
+    
+    return hour + " soat " + minute + " minut " + second + " sekund"; 
+  }
+  
+  function calculateCarTime (){
+    var hour = Math.floor(elInputValue / speedCar);
+    var minute = Math.floor((elInputValue / speedCar - hour) * 60);
+    var second = Math.floor(((elInputValue / speedCar - hour) * 60 - minute) * 60);
+    
+    return hour + " soat " + minute + " minut " + second + " sekund"; 
+  }
+  
 
-function resultPersonTime (){
-  return elInputValue / speedPerson;
-}
-
-function resultBicycleTime (){
- return elInputValue / speedBicycle;
-}
-
-function resultCarTime() {
-  return elInputValue / speedCar;
-}
-function resultAirplanTime() {
-  return elInputValue / speedAirplane;
-}
-
-elResultPerson.textContent = (resultPersonTime().toFixed(2));
-elResultBicycle.textContent = (resultBicycleTime()).toFixed(2);
-elResultCar.textContent =(resultCarTime()).toFixed(2);
-elResultAirplane.textContent = (resultAirplanTime()).toFixed(2);
-
-
+  function calculateAirplaneTime (){
+    var hour = Math.floor(elInputValue / speedAirplane);
+    var minute = Math.floor((elInputValue / speedAirplane - hour) * 60);
+    var second = Math.floor(((elInputValue / speedAirplane - hour) * 60 - minute) * 60);
+    
+    return hour + " soat " + minute + " minut " + second + " sekund"; 
+  }
+  
+  
+  
+  elResultPerson.textContent = calculatePersonTime ();
+  elResultBicycle.textContent = calculateBicycleTime();
+  elResultCar.textContent = calculateCarTime ();
+  elResultAirplane.textContent = calculateAirplaneTime ();
+  
+  
 })
